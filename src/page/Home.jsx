fa9aa01e083cb3fb984c48a2afd29354b478b42f
taoufik ahjam <taoufikahjam@gmail.com>
2022-04-13 01:03:26 +0200

    add initialisation value and performance it by useCollback

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,37 +1,41 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { decrease, increase} from '../context/redux/CounterSlice';
+import { decrease, increase } from '../context/redux/CounterSlice';
 import { toggleCounter } from '../context/redux/ToggleSlice';
 
 const Home = () => {
   const dispatch = useDispatch();
-
-
-  const conditionshow=(value)=>{
-      if (value === 0) {
-        return 'zero';
-      }
-      return value;
-  }
-
-
   const stats = useSelector((state) => state);
- 
 
-  const handleAction = (type,val) => {
-     if(type==='inc'){
-        dispatch(increase(val));
-     }
-     if(type==='dec'){
-        dispatch(decrease(val));
-     }
-    
+
+  const handleAction = useCallback((type, val) => {
+    if (type === 'inc') {
+      dispatch(increase(val));
+    }
+    if (type === 'dec') {
+      dispatch(decrease(val));
+    }
+  }, [dispatch]);
+
+
+useEffect(() => {
+    handleAction('inc', 35)
+}, [handleAction])
+
+
+  const conditionshow = (value) => {
+    if (value === 0) {
+      return 'zero';
+    }
+    return value;
   };
+
 
 
   const toggleCounterr = () => {
-    dispatch(toggleCounter() );
+    dispatch(toggleCounter());
   };
+  
 
   return (
     <>
@@ -40,7 +44,6 @@ const Home = () => {
 
       {stats.toggle.chowCounter && (
         <div>
-
           <p>{conditionshow(stats.Counter.count)}</p>
 
           <button onClick={() => handleAction('inc', 1)}>+</button>
